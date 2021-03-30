cb71a5f71dadd605a81da7ae1d6dba926ba1a098
Taylor Phillips <taylorstone.p@gmail.com>
2021-03-30 16:01:05 +0100

    Calendar

--- SalonManagement_GUI/src/main.cpp
+++ SalonManagement_GUI/src/main.cpp
@@ -7,12 +7,17 @@
 #endif
 #include <CustomSetup.h>
 #include "debuglogger.h"
-
+#include "basePanel.h"
+#include "wx/calctrl.h"
 bool MyApp::OnInit()
 {
     Main_Event_Window* frame = new Main_Event_Window(this);
     frame->Show(true);
 
+    SubFrame* cal = new SubFrame(NULL,400,400,tp_colour_menus::menu_white);
+    sm_BasePanel* main = new sm_BasePanel(cal,wxDefaultSize,tp_colour_menus::menu_black);
+    wxCalendarCtrl* calendar = new wxCalendarCtrl(main,wxID_ANY);
+    cal->Show(true);
     //LOG_PRINT_LINE("Hello");
 
     return true;
